faa906c554642dfffd7a1fbb27910979732bcc41
André Lima <dj_andre_lima@hotmail.com>
2022-08-08 02:00:04 -0300

    Preparando Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { BodyComponent } from './component/body/body.component';
+import { NavComponent } from './component/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
